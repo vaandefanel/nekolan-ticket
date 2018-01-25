4e2c8e97c30a5b289ce859ee1d9b863b535a750c
David Sagnette <dsagnette@vous.lu>
2018-01-25 19:48:56 +0100

    add parallax + ajax to php (charge client)

--- js/index.js
+++ js/index.js
@@ -1,63 +1,84 @@
-(function() {
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+$(document).ready(function($){
 	'use strict';
+	
+	var parallaxBox = $( '#box-parallax' );
 
-	// Create a Stripe client
-	var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+	function mouseParallax ( id, left, top, mouseX, mouseY, speed ) {
+		var obj = $( id );
+		
 
-	// Create an instance of Elements
-	var elements = stripe.elements();
+		var parentObj = obj.parent();
+		var containerWidth = parentObj.outerWidth();
+		var containerHeight = parentObj.outerHeight();
 
-	// Custom styling can be passed to options when creating an Element.
-	// (Note that this demo uses a wider set of styles than the guide below.)
-	var style = {
-	  base: {
-	    color: '#32325d',
-	    lineHeight: '18px',
-	    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-	    fontSmoothing: 'antialiased',
-	    fontSize: '16px',
-	    '::placeholder': {
-	      color: '#aab7c4'
-	    }
-	  },
-	  invalid: {
-	    color: '#fa755a',
-	    iconColor: '#fa755a'
-	  }
+		var winCenterX=$(window).outerWidth()/2;
+		var winCenterY=$(window).outerHeight()/2;
+
+		obj.css({left: ((winCenterX - (obj.outerWidth()/2) ) + left*winCenterX) + (mouseX - winCenterX - (obj.outerWidth()/2) )*speed   +  'px'});
+		obj.css({top: ((winCenterY - (obj.outerHeight()/2) ) + top*winCenterY) + (mouseY - winCenterX - (obj.outerWidth()/2) )*speed   + 'px'});
+	}
+
+	$('body').mousemove( function ( event ) {
+		event = event || window.event;
+		var x = event.pageX,
+		y = event.pageY;
+		applyparallax(x,y);
+
+		//mouseParallax ( 'l2', c2left, c2top, x, y, 15 );
+		//mouseParallax ( 'l3', c3left, c3top, x, y, 30 );
+		//mouseParallax ( 'l4', c4left, c4top, x, y, 65 );
+	});
+
+	function applyparallax(x,y)
+	{
+		mouseParallax ( '#sun',  0, 0, x, y, 50 );
+		mouseParallax ( '#flare_1',  0.4, -0.4, x, y, -0.008 );
+		mouseParallax ( '#planet_1',  -0.6, 0.4, x, y, 0.05 );
+		mouseParallax ( '#planet_2',  -0.85, -0.85, x, y, 0.005 );
+	}
+
+	$( window ).resize(function() {
+		$(document).trigger('click'); 
+	});
+
+	applyparallax(0,0);
+
+	var fct_charge_client=function(){
+
 	};
 
-	// Create an instance of the card Element
-	var card = elements.create('card', {style: style});
 
-	// Add an instance of the card Element into the `card-element` <div>
-	card.mount('#card-element');
+	$(window).bind('beforeunload', function(){
 
-	// Handle real-time validation errors from the card Element.
-	card.addEventListener('change', function(event) {
-	  var displayError = document.getElementById('card-errors');
-	  if (event.error) {
-	    displayError.textContent = event.error.message;
-	  } else {
-	    displayError.textContent = '';
-	  }
-	});
+		var data = $('form').serializeArray();
 
-	// Handle form submission
-	var form = document.getElementById('payment-form');
-	form.addEventListener('submit', function(event) {
-	  event.preventDefault();
+		$.ajax({type: 'POST',
+				url:'https://wt-928c20363e48533aab7b42b81b5ece88-0.run.webtask.io/stripe-payment/payment?currency=EUR&amount=2000&description=NekoLAN3',
+				data: data,
+				async: false
+		})
+		.done(function( result ) {
+			$("main").html('<div class="alert alert-success" role="alert">'+result+'</div>');
+		})
+		.fail(function( xhr, status, errorThrown ) {
+			$("main").html('<div class="alert alert-danger" role="alert">Désolé, il y a eu un problème !</div>');
+			console.log( "Error: " + errorThrown );
+			console.log( "Status: " + status );
+			console.dir( xhr );
+		});
 
-	  stripe.createToken(card).then(function(result) {
-	    if (result.error) {
-	      // Inform the user if there was an error
-	      var errorElement = document.getElementById('card-errors');
-	      errorElement.textContent = result.error.message;
-	    } else {
-	      // Send the token to your server
-	      stripeTokenHandler(result.token);
-	    }
-	  });
+		return "";
+
 	});
 
 
-})();
+});
